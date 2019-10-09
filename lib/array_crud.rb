8af2df45205abcae9016f6c5bf75e5433d780495
Abraham Moreno <adiaz2821@gmail.com>
2019-10-09 14:23:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   animals = ["dogs", "cats"]
-  animals.push("monkey")
+  animals.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
