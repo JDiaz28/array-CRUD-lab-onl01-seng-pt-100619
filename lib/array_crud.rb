bd81940850f124b9620fe85bc79df2f5a0b12f21
Abraham Moreno <adiaz2821@gmail.com>
2019-10-09 14:26:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   famous_people = ["RDJ", "Joaquin Phoenix"]
-  famous_people.unshift("Scarlett Johanson")
+  famous_people.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
